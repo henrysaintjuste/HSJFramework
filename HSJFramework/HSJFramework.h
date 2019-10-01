2d0981f51c9a59dae826a011985b4e86f2081815
Henry Saint-Juste <henrysaintjuste@dswinc.com>
2019-10-01 15:27:01 -0400

    Initial Commit

---
+++ HSJFramework/HSJFramework.h
@@ -0,0 +1,19 @@
+//
+//  HSJFramework.h
+//  HSJFramework
+//
+//  Created by Henry Saint-Juste on 10/1/19.
+//  Copyright © 2019 DSW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HSJFramework.
+FOUNDATION_EXPORT double HSJFrameworkVersionNumber;
+
+//! Project version string for HSJFramework.
+FOUNDATION_EXPORT const unsigned char HSJFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSJFramework/PublicHeader.h>
+
+
